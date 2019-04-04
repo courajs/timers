b6742e1ca3a7634960067cf5e85011690e39e067
Aaron Sikes <aaron@sikes.io>
2019-04-04 15:17:00 -0400

    v1

--- app/services/state.js
+++ app/services/state.js
@@ -1,16 +1,54 @@
 import Service from '@ember/service';
 import {tracked} from '@glimmer/tracking';
+import {action} from '@ember/object';
 
 import Timer from 'timers/lib/timer';
 
 export default class State extends Service {
   @tracked timers;
   @tracked current;
+  @tracked running = false;
+  clearToken = null;
 
   constructor() {
     super(...arguments);
     this.load();
     this.current = this.timers[0];
+
+    setInterval(() => this.save(), 5000);
+    window.addEventListener('beforeunload', () => this.save());
+  }
+
+  /*
+   *
+   * truth table
+   *
+   * running   same timer    start?    stop?
+   *
+   */
+
+  @action
+  toggle(timer) {
+    if (this.running && timer === this.current) {
+      this.stop();
+    } else {
+      this.start(timer);
+    }
+  }
+
+  start(timer) {
+    this.stop();
+    this.current = timer;
+    this.clearToken = setInterval(() => {
+      this.current.total++;
+    }, 1000);
+    this.running = true;
+  }
+
+  stop() {
+    clearInterval(this.clearToken);
+    this.clearToken = null;
+    this.running = false;
   }
 
   load() {
